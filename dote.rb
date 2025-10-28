84ac64c2e15cbb343bccc44a7d66f6c5b6789951
goldfishstar0055 <kmc2542@kamiyama.ac.jp>
2025-10-28 11:30:29 +0900

    Update SHA256 checksum

--- dote.rb
+++ dote.rb
@@ -1,8 +1,8 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/goldfishstar0055/dote"
-  url "https://github.com/goldfishstar0055/dote/archive/refs/tags/v1.0.0.tar.gz
-  sha256 "93ac910c597c17966b69b092466798895d73987811da3371276520a8564a4922"
+  homepage "https://github.com/goldfishstar0055/homebrew-dote"
+  url "https://github.com/goldfishstar0055/homebrew-dote/main/dote.c
+  sha256 "93ac910c597c17966b69b092466798895d73987811da3371276520a8564a4922
   version "1.0.0”
 
   def install
