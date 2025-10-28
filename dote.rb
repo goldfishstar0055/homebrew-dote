4897d6254389e7c39da55a482cf7936e8a955e61
goldfishstar0055 <kmc2542@kamiyama.ac.jp>
2025-10-28 11:43:04 +0900

    version "1.0.0"

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/goldfishstar0055/homebrew-dote"
-  url "https://github.com/goldfishstar0055/homebrew-dote/main/dote.c"
+  url "https://raw.githubusercontent.com/goldfishstar0055/homebrew-dote/main/dote.c"
   sha256 "93ac910c597c17966b69b092466798895d73987811da3371276520a8564a4922"
   version "1.0.0"
 
