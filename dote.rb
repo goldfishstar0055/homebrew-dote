b522b3d8c4a7a4284e1770e5ec242ce4f21a6e15
goldfishstar0055 <kmc2542@kamiyama.ac.jp>
2025-10-28 11:05:35 +0900

    version “1.0.0”

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/goldfishstar0055/homebrew-dote"
   url "https://raw.githubusercontent.com/goldfishstar0055/homebrew-dote/main/dote.c"
   sha256 "93ac910c597c17966b69b092466798895d73987811da3371276520a8564a4922"
+  version “1.0.0”
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
